9142b3b9584e4336702fb705c0c05b6bf2f80427
Kenyi Hancco <kenyihq@gmail.com>
2022-05-26 18:18:55 -0500

    Logout added

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
 
+    if (isset($_SESSION['user_id'])) {
+        header("Location: index.php");
+    }
+
     require "database.php";
 
     if (!empty($_POST['email']) && !empty($_POST['password'])){
