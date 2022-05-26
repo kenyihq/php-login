c041babc40a6f8cab24bfe8c8c2da670121ecf71
Kenyi Hancco <kenyihq@gmail.com>
2022-05-26 18:09:08 -0500

    Login is functional

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <?php
+    session_start();
+    require "database.php";
+
+    if (isset($_SESSION['user_id'])) {
+        $sql = "SELECT id, email, password FROM user WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $_SESSION['user_id']);
+        $stmt->execute();
+
+        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $user = null;
+
+        if (count($results) > 0) {
+            $user = $results;
+        }
+
+    }
+?>
+
+<?php
     $page_title = "Rumbos y Mundos";
     include("partials/header.php");
 ?>
 <div class="container">
+
+<?php if(!empty($user)): ?>
+
+    <p>Welcome <?= $user['email'] ?></p>
+    <a href="logout.php">Logout</a>
+
+<?php else: ?>
+
     <h1>Please Login or SingUp</h1>
     <a href="login.php">LogIn</a>
     <a href="signup.php">SignUp</a>
+        
+<?php endif; ?>
 </div>
 
 <?php
--- login.php
+++ login.php
@@ -1,9 +1,37 @@
 <?php
+    session_start();
+
+    require "database.php";
+
+    if (!empty($_POST['email']) && !empty($_POST['password'])){
+        $sql = "SELECT id, email, password FROM user WHERE email = :email && status = 1";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->execute();
+
+        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $message = '';
+
+        if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+            $_SESSION['user_id'] = $results['id'];
+            header("Location: index.php");
+        } else {
+            $message = "Invalid credentials";
+        }
+    }
+?>
+
+<?php
     $page_title = "Login";
     #$h1_title = "Login";
     include("partials/header.php");
 ?>
 <div class="container">
+    <?php if(!empty($message)): ?>
+        <p><?= $message ?></p>
+    <?php endif; ?>
+
     <h1>Login</h1>
     <span>or <a href="signup.php">SignUp</a></span>
     
