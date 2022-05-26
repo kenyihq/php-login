39b7e45dfbe30a26275d91c021db83fcf75688f5
Kenyi Hancco <kenyihq@gmail.com>
2022-05-26 14:14:18 -0500

    Register first user in database successfully

--- signup.php
+++ signup.php
@@ -1,7 +1,21 @@
 <?php
-
     require "database.php";
 
+    $message = '';
+
+    if (!empty($_POST['email']) && !empty($_POST['password'])) {
+        $sql = "INSERT INTO user (email, password) VALUES (:email, :password)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':email', $_POST['email']);
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $stmt->bindParam(':password', $password);
+
+        if ($stmt->execute()) {
+            $message = "Successfully created new user";
+        } else {
+            $message = "Error creating new user";
+        }
+    }
 ?>
 
 
@@ -10,11 +24,17 @@
     #$h1_title = "Login";
     include("partials/header.php");
 ?>
+
+<?php if(!empty($message)): ?>
+    <p><?php $message ?></p>
+<?php endif; ?>
+
+
 <div class="container">
     <h1>SingUp</h1>
     <span>or <a href="login.php">Login</a></span>
     
-    <form action="login.php" method="POST">
+    <form action="signup.php" method="POST">
         <input type="email" name="email" placeholder="Enter your email">
         <input type="password" name="password" placeholder="Enter your password">
         <input type="password" name="confirm_password" placeholder="Confirm your password">
