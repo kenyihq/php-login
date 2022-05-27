083bba380ab34eb5ffbc12af8f6e7ee5c4c4ec67
Kenyi Hancco <kenyihq@gmail.com>
2022-05-26 19:59:37 -0500

    Validate user

--- login.php
+++ login.php
@@ -8,21 +8,30 @@
     require "database.php";
 
     if (!empty($_POST['email']) && !empty($_POST['password'])){
+        
         $sql = "SELECT id, email, password FROM user WHERE email = :email && status = 1";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':email', $_POST['email']);
         $stmt->execute();
 
-        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($stmt->rowCount() == 0){
+            $message = "User not found";
+        }else{
 
-        $message = '';
-
-        if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-            $_SESSION['user_id'] = $results['id'];
-            header("Location: index.php");
-        } else {
-            $message = "Invalid credentials";
+            $results = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+            $message = '';
+    
+            if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+                $_SESSION['user_id'] = $results['id'];
+                header("Location: index.php");
+            } else {
+                $message = "Invalid credentials";
+            }
         }
+
+    } else {
+        $message = "Please enter your credentials";
     }
 ?>
 
